4e68e8832b9ba7aca434bf2db9b8dcf57aaa2bbf
Sean Young <seandayoungone@gmail.com>
2017-06-26 22:26:29 +0200

    not counting cols and rows correctly

--- src/main.c
+++ src/main.c
@@ -20,13 +20,13 @@
 #include "../libft/get_next_line.h"
 #include "fdf.h"
 
-int exit_func(int keycode, void *param)
+/*int exit_func(int keycode, void *param)
 {
 	//printf("%d\n", keycode);
 	if (keycode == 53)
 		mlx_destroy_window(param, param + 1);
 	return (0);
-}
+	}*/
 void	rotate(void *mlx, void *win, int x, int y, int color)
 {
 	float x1 = (float)x;
@@ -58,63 +58,72 @@ int		main(int argc, char **argv)
 	int		y;
 	int		gap;
 	int		fd;
+	int		fd_2;
 	char	*row;
-	int		map[100][100];
 	char	**split;
 	int		row_count;
 	int		col_count;
-	int		len;
-	int		height;
 	int		xyh0;
 	int		xyh1;
 	int		xyh2;
 	int		xyh3;
 	int		multi;
-	void	*winmlx[2];
+	char		buff[9];
 
-	multi = 2;
-	height = 0;
-	grid cell[50][50];
-
+	multi = 1;
+	grid	**cell;
 	mlx = mlx_init();
-	win = mlx_new_window(mlx, 1000, 1000, "42");
-	winmlx[0] = mlx;
-	winmlx[1] = win;
-	//mlx_loop(mlx);
+	win = mlx_new_window(mlx, 500, 500, "42");
 	x = 0;
 	y = 50;
 	gap = 10;
-	row_count = 0;
+	row_count = 1;
 	col_count = 0;
 	if (argc != 2)
 	{
 		write(1, "Wrong number of arguments.\n", 28);
 		return (0);
 	}
+	fd_2 = open(argv[1], O_RDONLY);
+	while (read(fd_2, buff, 1))
+	{
+		if (buff[0] == '\n')
+			break ;
+		col_count++;
+	}
+	row_count++;
+	while (read(fd_2, buff, 1))
+	{
+		if (buff[0] == '\n')
+			row_count++;
+	}
+		printf("### Row Count:%d\n### Col Count: %d\n", row_count, col_count);
 	fd = open(argv[1], O_RDONLY);
+	cell = (grid **)malloc(sizeof(grid *) * row_count);
+	for (int i = 0; i < col_count; i++)
+		cell[i] = (grid *)malloc(sizeof(grid) * col_count);
+	row_count = 0;
+	col_count = 0;
 	while (get_next_line(fd, &row))
 	{
+		
 		split = ft_strsplit((const char*)row, ' ');
 		col_count = 0;
 		while(split[col_count] != NULL)
 		{
-			map[row_count][col_count] = ft_atoi((const char*)split[col_count]);
+			cell[row_count][col_count].height = ft_atoi((const char*)split[col_count]);
 			col_count++;
 		}
 		row_count++;
-		free(split);
 	}
-//	grid **cell = (grid **)malloc(sizeof(grid *) * row_count);
-//	for (int i = 0; i < col_count; i++)
-//		cell[i] = (grid *)malloc(sizeof(grid) * col_count);
-	for (int i = 0; i < row_count; i++)
-		for (int j = 0; j < col_count; j++)
-			cell[i][j].height = map[i][j];
+	row_count--;
+	col_count--;
+	printf("### Row Count:%d\n### Col Count: %d\n", row_count, col_count);
 	for (int i = 0; i < row_count; i++)
 	{
 		y += gap;
-		x = 400;
-		for (int j = 0; j < (col_count); j++)
+		x = 200;
+		for (int j = 0; j < col_count; j++)
 		{
 			xyh0 = 0;
 			xyh1 = 0;
@@ -157,6 +166,8 @@ int		main(int argc, char **argv)
 					xyh2 = xyh0;
 					xyh3 = xyh0;
 				}
+
+			
 			}
 			x += gap;
 			cell[i][j].x0 = x - xyh0 * multi;
@@ -190,10 +201,10 @@ int		main(int argc, char **argv)
 
 		}
 	}
-	int (*funcpointer)(int, void*);
-	funcpointer = &exit_func;
-	int keycode = 0;
-	mlx_key_hook(win, *(funcpointer)(keycode, winmlx), 0);
+	//int (*funcpointer)(int, void*);
+	//funcpointer = &exit_func;
+	//int keycode = 0;
+	//mlx_key_hook(win, *(funcpointer)(keycode, winmlx), 0);
 	mlx_loop(mlx);
 	return (0);
 }
